65f144e9b5d8fc3adc4134480d0485ab38000d45
Rudolph <rudyjm3@yahoo.com>
2020-04-29 10:45:46 -0400

    added data-framework to images to setup matching code in js. Added matching cards as well to html.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
   this.classList.toggle('flip');
-}
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+  if (!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    // do cards match?
+    console.log(firstCard.dataset.framework);
+    console.log(secondCard.dataset.framework);
+  }
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
